76a90a4c9e62c28f5955ffe568bc6fb34275fb8f
XieSXi <1547981174@qq.com>
2019-06-22 00:30:35 +0800

    增加搜索功能

--- app/src/main/java/com/example/administrator/afinal/MoreActivity.java
+++ app/src/main/java/com/example/administrator/afinal/MoreActivity.java
@@ -9,17 +9,27 @@ import android.widget.Button;
 
 public class MoreActivity extends AppCompatActivity {
 
-    Button weabtn;
+    Button weabtn;Button sousuo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more);
 
         weabtn=(Button)findViewById(R.id.weatherbtn);
+        sousuo=(Button)findViewById(R.id.sousuobtn);
         weabtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent= new Intent(getApplicationContext(),WeatherActivity.class);//打开另一个Activity
+                Bundle bundle1 = new Bundle();
+                startActivity(intent);
+
+            }
+        });
+        sousuo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(),SuoActivity.class);//打开另一个Activity
                 Bundle bundle1 = new Bundle();
                 startActivity(intent);
 
---
+++ app/src/main/java/com/example/administrator/afinal/SuoActivity.java
@@ -0,0 +1,43 @@
+package com.example.administrator.afinal;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SuoActivity extends AppCompatActivity {
+
+    String username;String content;
+    EditText sousuo;Button tijiao;
+    String TAG="sousuo";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suo);
+
+        SharedPreferences mySharedPreferences= getSharedPreferences("userinfo",Activity.MODE_PRIVATE);
+        username=mySharedPreferences.getString("username","");
+        sousuo=(EditText)findViewById(R.id.sousuokuang);
+        tijiao=(Button)findViewById(R.id.tijiao);
+        content=sousuo.getText().toString().trim();
+        tijiao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(),XiangqingActivity.class);//打开另一个Activity
+                Bundle bundle = new Bundle();
+                bundle.putString("username",username);
+                bundle.putString("hdname","义卖旧书籍");
+                Log.i(TAG,""+username+content);
+                intent.putExtras(bundle);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
